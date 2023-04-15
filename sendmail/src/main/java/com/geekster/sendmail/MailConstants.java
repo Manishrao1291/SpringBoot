6757b7c30470bd46878738bbc7e0735d9550a5b1
Manish <manishrao1291@gmail.com>
2023-04-15 20:30:46 +0530

    send mail

---
+++ sendmail/src/main/java/com/geekster/sendmail/MailConstants.java
@@ -0,0 +1,17 @@
+package org.geekster;
+
+public class MailConstants {
+
+    //sender email
+    public static final String SENDER ="manishrao1291@gmail.com";
+
+    // receiver email
+    public static final String RECEIVEREMAILADDRESS = "manishrao255@gmail.com";
+
+    // Subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 15-April-2023";
+
+    // Message to be send in mail body
+    public static final String MESSAGE  = "This mail is sent by using java api. BY Manish";
+
+}
